95cc40aca16e50ff76b4b305a465a3a9c8c9d204
Ismail Hatti <ismail.hatti10@gmail.com>
2025-06-16 11:15:53 +0200

    feat: 1st attempt using random forest

--- spaceship-titanic/model.py
+++ spaceship-titanic/model.py
@@ -67,3 +67,70 @@ except KeyError:
     print("Field does not exist")
 
 print(dataset_df.head(5))
+
+# -- Splitting --
+def split_dataset(dataset, test_ratio=0.20):
+  test_indices = np.random.rand(len(dataset)) < test_ratio
+  return dataset[~test_indices], dataset[test_indices]
+
+train_ds_pd, valid_ds_pd = split_dataset(dataset_df)
+print("{} examples in training, {} examples in testing.".format(
+    len(train_ds_pd), len(valid_ds_pd)))
+
+# -- Converting to TF Datasets --
+train_ds = tfdf.keras.pd_dataframe_to_tf_dataset(train_ds_pd, label=label)
+valid_ds = tfdf.keras.pd_dataframe_to_tf_dataset(valid_ds_pd, label=label)
+
+
+
+rf = tfdf.keras.RandomForestModel()
+rf.compile(metrics=["accuracy"]) # setting metric to be evaluated 
+
+# -- Training --
+rf.fit(x=train_ds)
+
+# colab = tfdf.model_plotter.plot_model_in_colab(rf, tree_idx=0, max_depth=3)
+
+
+# -- Evaluation --
+evaluation = rf.evaluate(x=valid_ds,return_dict=True)
+
+for name, value in evaluation.items():
+  print(f"{name}: {value:.4f}")
+
+inspector = rf.make_inspector()
+inspector.variable_importances()["NUM_AS_ROOT"]
+
+# SUBMISSION
+# Load the test dataset
+test_df = pd.read_csv('data/test.csv')
+submission_id = test_df.PassengerId
+
+# Replace NaN values with zero
+test_df[['VIP', 'CryoSleep']] = test_df[['VIP', 'CryoSleep']].fillna(value=0)
+
+# Creating New Features - Deck, Cabin_num and Side from the column Cabin and remove Cabin
+test_df[["Deck", "Cabin_num", "Side"]] = test_df["Cabin"].str.split("/", expand=True)
+test_df = test_df.drop('Cabin', axis=1)
+
+# Convert boolean to 1's and 0's
+test_df['VIP'] = test_df['VIP'].astype(int)
+test_df['CryoSleep'] = test_df['CryoSleep'].astype(int)
+
+# Convert pd dataframe to tf dataset
+test_ds = tfdf.keras.pd_dataframe_to_tf_dataset(test_df)
+
+# Get the predictions for testdata
+predictions = rf.predict(test_ds)
+
+n_predictions = (predictions > 0.5).astype(bool)
+
+output = pd.DataFrame({'PassengerId': submission_id,
+                       'Transported': n_predictions.squeeze()})
+
+# print(output)
+
+sample_submission_df = pd.read_csv('sample_submission.csv')
+sample_submission_df['Transported'] = n_predictions
+sample_submission_df.to_csv('submission.csv', index=False)
+sample_submission_df.head()
